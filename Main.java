1aba249cdff9a5c4dbe35c6b9b6e532dca1852d2
nakhuong269 <75614193+nakhuong269@users.noreply.github.com>
2022-07-04 15:43:13 +0700

    Commit find SlangWord

--- Main.java
+++ Main.java
@@ -5,10 +5,14 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
+import java.io.*;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Objects;
 
 public class Main extends JPanel implements ActionListener{
-    JPanel pn1, pn2, pn3, pn4, pn5, pn6;
-
+    JPanel pn1, pn2, pn3, pn4, pn5, pn6, pnCont , pn7  ;
     JButton btnAdd, btnEdit, btnDelete, btnSearch, btnSearchDefinition, btnRandom, btnReset, btnHome, btnGame;
     JTextField tfSearch;
     BoxLayout bl1, bl2;
@@ -16,16 +20,17 @@ public class Main extends JPanel implements ActionListener{
     JLabel lb22;
     BorderLayout bdl1;
     FlowLayout fl1;
+    CardLayout cl;
 
+    static HashMap<String, String> map;
 
-
-    public static void main(String args[])
-    {
+    public static void main(String args[]) throws FileNotFoundException {
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
             public void run() {
                 createAndShowGUI();
             }
         });
+        initData();
     }
 
     public Main()
@@ -38,12 +43,14 @@ public class Main extends JPanel implements ActionListener{
 
 
 
-        tfSearch = new JTextField();
+        tfSearch = new JTextField(20);
         pn2 = new JPanel();
         bl2 = new BoxLayout(pn2, BoxLayout.X_AXIS);
         pn2.setLayout(bl2);
         pn2.setBorder(new EmptyBorder(0, 0,15,0));
         btnSearch = new JButton("Tìm kiếm chính xác");
+        btnSearch.addActionListener(this);
+        btnSearch.setActionCommand("btnSearch");
         btnSearchDefinition = new JButton("Tìm kiếm liên quan");
 
         pn2.add(tfSearch);
@@ -74,22 +81,39 @@ public class Main extends JPanel implements ActionListener{
         pn1.add(pn2);
         pn1.add(pn3);
 
-        pn4 = new JPanel(new CardLayout());
+        pn4 = new JPanel();
+        cl = new CardLayout();
+
+        pn4.setLayout(cl);
+
+        pn7 = new JPanel();
         bdl1 = new BorderLayout();
-        pn4.setLayout(bdl1);
-        pn4.add(pn1,BorderLayout.PAGE_START);
+        pn7.setLayout(bdl1);
+        pn7.add(pn1,BorderLayout.PAGE_START);
+        pn4.add(pn7);
 
         pn6 = new JPanel();
         btnHome = new JButton("Trang chủ");
+        btnHome.addActionListener(this);
+        btnHome.setActionCommand("btnHome");
+
         btnGame = new JButton("Đố vui");
+        btnGame.addActionListener(this);
+        btnGame.setActionCommand("btnGame");
         pn6.setBorder(new EmptyBorder(0,0,20,0));
         pn6.add(btnHome);
         pn6.add(btnGame);
 
+        pnCont = new JPanel();
+        pnCont.setLayout(cl);
+        pnCont.add(pn4,"Home");
+
+        pn5 = new JPanel();
+        pn5.setBackground(Color.BLACK);
+        pnCont.add(pn5,"Game");
 
         add(pn6,BorderLayout.PAGE_START);
-        add(pn4, BorderLayout.CENTER);
-
+        add(pnCont, BorderLayout.CENTER);
     }
     private static void createAndShowGUI() {
         //Make sure we have nice window decorations.
@@ -115,7 +139,46 @@ public class Main extends JPanel implements ActionListener{
 
     @Override
     public void actionPerformed(ActionEvent e) {
-
+        String str = e.getActionCommand();
+        if(str.equals("btnHome"))
+        {
+            cl.show(pnCont,"Home");
+            System.out.println("Home");
+        } else if (str.equals("btnGame")) {
+            cl.show(pnCont,"Game");
+            System.out.println("Game");
+        }
+        else if(str.equals("btnSearch"))
+        {
+            System.out.println(findSlangWord(tfSearch.getText()));
+        }
     }
 
+    private static void initData() throws FileNotFoundException {
+        String url = "slang.txt";
+
+        File file = new File(url);
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+
+        try {
+            String line = reader.readLine();
+            map = new HashMap<String,String>();
+            while (line != null) {
+                String[] w = line.split("`");
+                map.put(w[0],w[1]);
+                line = reader.readLine();
+            }
+        }catch (Exception e)
+        {
+            System.out.print(e.getMessage());
+        }
+    }
+
+    private String findSlangWord(String slang)
+    {
+        if(map.containsKey(slang)) {
+            return map.get(slang);
+        }
+        return "";
+    }
 }
